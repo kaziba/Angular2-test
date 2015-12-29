55f5eb4c2fafda8ab56a3e21de8ff084d2f63124
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2015-12-29 19:39:49 +0900

    [Done] - Services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
-
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -65,13 +65,25 @@ import {HeroDetailComponent} from './hero-detail.component';
       border-radius: 4px 0px 0px 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Tour of Heroes';
-  public heroes = HEROES;
+  public heroes: Hero[];
   public selectedHero: Hero;  // ただの型宣言
+
+  constructor(private _heroService: HeroService) {}
+
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
